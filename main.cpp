528839fa27c6e9763d248effce1997e18f865b76
v3nividiv1ci <v3nividiv1ci@outlook.com>
2021-07-23 14:49:38 +0800

    A Type func completed (finally..

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <string>
 #include <cerrno>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -55,10 +56,10 @@ void SetHead (DNSHeader* header) {
 void SetQuery (char *name, unsigned char *buf, int len) {
     memcpy(buf + 12, name, len); // 首部长12字节
     int pos = len + 12;
-    for (int i = 0; i < pos; i ++) {
-        printf("%x.", buf[i]);
-    }
-    printf("\n");
+//    for (int i = 0; i < pos; i ++) {
+//        printf("%x.", buf[i]);
+//    }
+//    printf("\n");
     buf[pos] = 0;
     /*
      * 下一步：查询类型根据输入更改
@@ -66,9 +67,9 @@ void SetQuery (char *name, unsigned char *buf, int len) {
     buf[pos + 1] = 1; // Type: 2 byte A类型为1
     buf[pos + 2] = 0;
     buf[pos + 3] = 1; // Class: 2 byte  IN为互联网地址
-    for (int i = 0; i < pos + 4; i ++) {
-        printf("%x.", buf[i]);
-    }
+//    for (int i = 0; i < pos + 4; i ++) {
+//        printf("%x.", buf[i]);
+//    }
 }
 
 // 将域名由www.xxxxx.yyy的形式变为 3www5xxxxx3yyy0
@@ -92,7 +93,7 @@ int ChangeDN (char *DN, char *name) {
     return (strlen(DN) + 2); // 比之前增加了2个字符
 }
 
-int SendRecvDNSPacket (unsigned char *buf, int len, char *RecvMsg) {
+int SendRecvDNSPacket (unsigned char *buf, int len, unsigned char *RecvMsg) {
     int s;
     struct sockaddr_in MySock{};
 
@@ -109,19 +110,59 @@ int SendRecvDNSPacket (unsigned char *buf, int len, char *RecvMsg) {
 
 }
 
-//int resolve(unsigned char *RecvMsg, int len, int LenRecv) {
-//
-//}
 
+int AnswerRRs(unsigned char* RecvMsg, int LenSend) {
+    short * ptr;
+    ptr = (short *)(RecvMsg + 7);
+    int AnsRRs = *ptr;
+    return AnsRRs;
+}
+
+int ParseMsg(unsigned char* RecvMsg, int LenSend, int RRs, int ** addr, int LenName) {
+    int pos = LenSend;
+    int AddrRRs = RRs;
+    // 遍历每条answer
+    /*
+     * answer的格式：
+     * Name:  2 byte || LenName
+     * Type:  2 byte
+     * Class: 2 byte
+     * TTL:   4 byte
+     * RDLength: 2 byte
+     * RData: RDLength byte
+     * */
+    for (int i = 0; i < RRs; i++) {
+        pos += LenName;
+        // 判断TYPE是否为A类型 (1)
+        // (但是我自己发的收到的并没有压缩...只有用dig的时候压缩了) Name 域名重复 故用指针压缩（见rfc1035 4.1.4 message compression）
+        int a = RecvMsg[pos + 1];
+        if ((int)RecvMsg[pos + 1] != 1) {
+            short * ptr;
+            ptr = (short *)(RecvMsg + (pos + 8));
+            LenName = ntohs(*ptr); // 这个地方一定要记得转换大小端..但是AnswerRRs却已经是主机字节序..太怪了
+            pos += 10 + LenName;
+            AddrRRs --;
+        }
+        else {
+            pos += 10;
+            for(int j = 0; j < 4; j++) {
+                addr[i][j] = RecvMsg[pos + j];
+            }
+            pos += 4;
+        }
+    }
+    return AddrRRs;
+}
 
 int main () {
     DNSHeader header{};
     unsigned char buf[MAX_SIZE]; // socket发送的数据
     char DN[MAX_SIZE]; // 要解析的域名：www.xxx.yyy
     char name[MAX_SIZE]; // 转换成的符合DNS报文格式的域名
-    char RecvMsg[MAX_SIZE]; // 接收的数据
+    unsigned char RecvMsg[MAX_SIZE]; // 接收的数据
     int LenSend = 0; // socket 发送数据的长度
     int LenRecv = 0; // socket 接收报文长度
+    int LenName = 0; // 域名长度
     int s;
 
     /*
@@ -133,18 +174,19 @@ int main () {
     // printf("%s", DN);
 
     LenSend = ChangeDN(DN, name);
-    printf("%d", LenSend);
-    for (int i = 0; i < LenSend; i ++) {
-        printf("%c.", name[i]);
-    }
+    LenName = LenSend;
+//    printf("%d", LenSend);
+//    for (int i = 0; i < LenSend; i ++) {
+//        printf("%c.", name[i]);
+//    }
     int j;
 
     // 填充首部
     SetHead(&header);
-    for (int i = 0; i < 12; i++) {
-        printf("%x", &header+i);
-    }
-    printf("%d", sizeof(header));
+//    for (int i = 0; i < 12; i++) {
+//        printf("%x", &header+i);
+//    }
+//    printf("%d", sizeof(header));
     memcpy(buf, &header, sizeof(header) );
     // 填充查询字段
     SetQuery(name, buf, LenSend);
@@ -153,12 +195,28 @@ int main () {
     memset(RecvMsg, 0, MAX_SIZE);
     LenRecv = SendRecvDNSPacket(buf, LenSend, RecvMsg);
 
-    printf("接收报文长度为 %d 字节\n", LenRecv);
-    printf("接收报文的16进制表示：\n");
-    for (int i = 0; i < LenRecv; i++) {
-        printf("%x.", (unsigned char)RecvMsg[i]);
+//    printf("接收报文长度为 %d 字节\n", LenRecv);
+//    printf("接收报文的16进制表示：\n");
+//    for (int i = 0; i < LenRecv; i++) {
+//        printf("%x.", (unsigned char)RecvMsg[i]);
+//    }
+
+
+    int RR = AnswerRRs(RecvMsg,LenSend);
+    int **addr = (int**)malloc(sizeof(int*) * RR);  //sizeof(int*),不能少*，一个指针的内存大小，每个元素是一个指针。
+    for (int i = 0;i < RR;i++)
+    {
+        addr[i] = (int*)malloc(sizeof(int) * 4);
     }
-    printf("\n%s的IP地址为：\n", DN);
+    int AddrRR = ParseMsg(RecvMsg, LenSend, RR, addr, LenName);
+    printf("%s的IP地址为：\n", DN);
+    for (int i = RR - AddrRR; i < RR; i++) {
+        printf("%d.%d.%d.%d\n", addr[i][0], addr[i][1], addr[i][2], addr[i][3]);
+    }
+
+    for (int i = 0;i < RR;i++)
+        free(addr[i]);
+    free(addr);
 
 
 }
